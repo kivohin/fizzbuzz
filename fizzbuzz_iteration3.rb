73ca09f93d10d1e0efc24a9d423fabe5bafd3a72
Anthony Nabuurs <kivohin@protonmail.com>
2016-03-15 21:16:12 +0100

    Solve and create FizzBuzz iteration 3 code

--- fizzbuzz_iteration3.rb
+++ fizzbuzz_iteration3.rb
@@ -1,2 +1,40 @@
 # FizzBuzz program, iteration 3
 
+numbers = 1
+
+while numbers <= 100
+	if numbers % 3 == 0 && numbers % 5 == 0
+		if numbers.to_s.include? "1"
+			puts "FizzBuzzBang"
+
+		else
+			puts "FizzBuzz"
+		end
+
+	elsif numbers % 5 == 0
+		if numbers.to_s.include? "1"
+			puts "BuzzBang"
+
+		else
+			puts "Buzz"
+		end
+
+	elsif numbers % 3 == 0
+		if numbers.to_s.include? "1"
+			puts "FizzBang"
+
+		else
+			puts "Fizz"
+		end
+		
+	else
+		if numbers.to_s.include? "1"
+			puts "Bang"
+
+		else
+			puts numbers
+		end		
+	end
+
+	numbers = numbers + 1
+end
